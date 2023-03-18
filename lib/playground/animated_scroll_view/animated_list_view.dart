ff8bf79ba7aaa24d1f280b9f8c155459523fbbd4
Criszx17Dev <criszx17@gmail.com>
2023-03-18 12:36:15 -0400

    first commit

---
+++ lib/playground/animated_scroll_view/animated_list_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:recipes_ui_module/playground/animated_scroll_view/animated_scroll_view_item.dart';
+import 'package:recipes_ui_module/playground/animated_scroll_view/list_item.dart';
+
+class AnimatedListView extends StatelessWidget {
+  const AnimatedListView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      cacheExtent: 0,
+      padding: const EdgeInsets.symmetric(horizontal: 17, vertical: 20),
+      itemCount: 100,
+      itemBuilder: (context, index) => const AnimatedScrollViewItem(child: ListItem()),
+    );
+  }
+}
